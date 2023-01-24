86671ec80dd740a34f417c688030b312e6448281
Nicolas Morales <nicolasjmoralesl@yahoo.com>
2023-01-24 09:24:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hola Mundo!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Servidor corriendo sobre http://${hostname}:${port}`);
+// });
+
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+ res.send('Hola Mundo!');
+});
+app.listen(3000, function() {
+ console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+}); 
